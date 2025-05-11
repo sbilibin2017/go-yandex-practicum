65b8d28c20469e7841b6aeccd185d7df5a5d8357
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-05-11 10:58:45 +0300

    Added andtested services converters helpers

--- internal/services/helpers.go
+++ internal/services/helpers.go
@@ -4,12 +4,44 @@ import (
 	"strings"
 
 	"github.com/fatih/structs"
+	"github.com/mitchellh/mapstructure"
 )
 
-func structToMap(v any) map[string]any {
+func structToMap[T any](v T) map[string]any {
 	m := structs.Map(v)
 	for k, v := range m {
+		delete(m, k)
 		m[strings.ToLower(k)] = v
 	}
 	return m
+}
+
+func mapToStruct[T any](data map[string]any) (T, error) {
+	var result T
+	config := &mapstructure.DecoderConfig{
+		TagName:     "mapstructure",
+		Result:      &result,
+		ErrorUnused: true,
+	}
+	decoder, _ := mapstructure.NewDecoder(config)
+	err := decoder.Decode(data)
+	if err != nil {
+		return result, err
+	}
+	return result, nil
+}
+
+func mapSliceToStructSlice[T any](data []map[string]any) ([]T, error) {
+	var result []T
+	for _, dataItem := range data {
+		item, err := mapToStruct[T](dataItem)
+		if err != nil {
+			return []T{}, err
+		}
+		result = append(result, item)
+	}
+	if result == nil {
+		result = []T{}
+	}
+	return result, nil
 }
--- internal/services/helpers_test.go
+++ internal/services/helpers_test.go
@@ -3,20 +3,61 @@ package services
 import (
 	"testing"
 
+	"github.com/mitchellh/mapstructure"
 	"github.com/stretchr/testify/assert"
 )
 
-type TestStruct struct {
-	FieldOne string
-	FieldTwo int
+type Person struct {
+	Name string `mapstructure:"name"`
+	Age  int    `mapstructure:"age"`
 }
 
 func TestStructToMap(t *testing.T) {
-	testStruct := TestStruct{
-		FieldOne: "Value1",
-		FieldTwo: 42,
+	person := Person{Name: "John", Age: 30}
+	result := structToMap(person)
+	assert.Equal(t, "John", result["name"])
+	assert.Equal(t, 30, result["age"])
+}
+
+func TestMapToStruct(t *testing.T) {
+	data := map[string]any{
+		"name": "Jane",
+		"age":  25,
 	}
-	result := structToMap(testStruct)
-	assert.Equal(t, "Value1", result["fieldone"], "fieldone должно быть равно Value1")
-	assert.Equal(t, 42, result["fieldtwo"], "fieldtwo должно быть равно 42")
+	person, _ := mapToStruct[Person](data)
+	assert.Equal(t, "Jane", person.Name)
+	assert.Equal(t, 25, person.Age)
+}
+
+func TestMapToStruct_DecoderInitError(t *testing.T) {
+	type Dummy struct{}
+	config := &mapstructure.DecoderConfig{
+		TagName:     "mapstructure",
+		Result:      nil,
+		ErrorUnused: true,
+	}
+	decoder, err := mapstructure.NewDecoder(config)
+	assert.Nil(t, decoder)
+	assert.Error(t, err)
+}
+
+func TestMapSliceToStructSlice(t *testing.T) {
+	data := []map[string]any{
+		{"name": "John", "age": 30},
+		{"name": "Jane", "age": 25},
+	}
+	people, err := mapSliceToStructSlice[Person](data)
+	assert.NoError(t, err)
+	assert.Len(t, people, 2)
+	assert.Equal(t, "John", people[0].Name)
+	assert.Equal(t, 30, people[0].Age)
+	assert.Equal(t, "Jane", people[1].Name)
+	assert.Equal(t, 25, people[1].Age)
+}
+
+func TestEmptyMapSliceToStructSlice(t *testing.T) {
+	data := []map[string]any{}
+	people, err := mapSliceToStructSlice[Person](data)
+	assert.NoError(t, err)
+	assert.Len(t, people, 0)
 }
