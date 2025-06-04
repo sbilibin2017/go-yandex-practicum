6b341b18aad5710ae5f6a868f1145995f0580388
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-06-04 09:43:16 +0300

    Added server, agent configurationwith config file

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"os"
 	"strconv"
+	"time"
 )
 
 const (
@@ -13,6 +15,8 @@ const (
 	flagKeyName            = "k"
 	flagRateLimitName      = "l"
 	flagCryptoKeyName      = "crypto-key"
+	flagConfigName         = "c"
+	flagConfigNameLong     = "config"
 
 	envAddress        = "ADDRESS"
 	envPollInterval   = "POLL_INTERVAL"
@@ -20,6 +24,7 @@ const (
 	envKey            = "KEY"
 	envRateLimit      = "RATE_LIMIT"
 	envCryptoKey      = "CRYPTO_KEY"
+	envConfig         = "CONFIG"
 
 	defaultAddress        = "http://localhost:8080"
 	defaultPollInterval   = 2
@@ -27,6 +32,7 @@ const (
 	defaultKey            = ""
 	defaultRateLimit      = 0
 	defaultCryptoKey      = ""
+	defaultConfig         = ""
 
 	descAddress        = "Metrics server address"
 	descPollInterval   = "Poll interval in seconds"
@@ -34,6 +40,7 @@ const (
 	descKey            = "Key for HMAC SHA256 hash"
 	descRateLimit      = "Max number of concurrent outgoing requests"
 	descCryptoKey      = "Path to public key file for encryption"
+	descConfig         = "Path to config file"
 )
 
 const (
@@ -50,17 +57,26 @@ var (
 	flagKey            string
 	flagRateLimit      int
 	flagCryptoKey      string
+	flagConfigPath     string
 )
 
-func parseFlags() {
+func parseFlags() error {
 	flag.StringVar(&flagServerAddress, flagAddressName, defaultAddress, descAddress)
 	flag.IntVar(&flagPollInterval, flagPollIntervalName, defaultPollInterval, descPollInterval)
 	flag.IntVar(&flagReportInterval, flagReportIntervalName, defaultReportInterval, descReportInterval)
 	flag.StringVar(&flagKey, flagKeyName, defaultKey, descKey)
 	flag.IntVar(&flagRateLimit, flagRateLimitName, defaultRateLimit, descRateLimit)
 	flag.StringVar(&flagCryptoKey, flagCryptoKeyName, defaultCryptoKey, descCryptoKey)
+	flag.StringVar(&flagConfigPath, flagConfigName, defaultConfig, descConfig)
+	flag.StringVar(&flagConfigPath, flagConfigNameLong, defaultConfig, descConfig)
 
 	flag.Parse()
+
+	err := loadConfigFile(flagConfigPath)
+
+	if err != nil {
+		return err
+	}
 
 	if env := os.Getenv(envAddress); env != emptyString {
 		flagServerAddress = env
@@ -86,4 +102,44 @@ func parseFlags() {
 	if env := os.Getenv(envCryptoKey); env != "" {
 		flagCryptoKey = env
 	}
+
+	return nil
+}
+
+func loadConfigFile(path string) error {
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	var cfg struct {
+		Address        string `json:"address"`
+		ReportInterval string `json:"report_interval"`
+		PollInterval   string `json:"poll_interval"`
+		CryptoKey      string `json:"crypto_key"`
+	}
+
+	if err := json.NewDecoder(f).Decode(&cfg); err != nil {
+		return err
+	}
+
+	if cfg.Address != "" {
+		flagServerAddress = cfg.Address
+	}
+	if cfg.ReportInterval != "" {
+		if d, err := time.ParseDuration(cfg.ReportInterval); err == nil {
+			flagReportInterval = int(d.Seconds())
+		}
+	}
+	if cfg.PollInterval != "" {
+		if d, err := time.ParseDuration(cfg.PollInterval); err == nil {
+			flagPollInterval = int(d.Seconds())
+		}
+	}
+	if cfg.CryptoKey != "" {
+		flagCryptoKey = cfg.CryptoKey
+	}
+
+	return nil
 }
